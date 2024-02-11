cab58b6a29e56310b2b7c6d5ffa8961cd5bd0c14
유찬규 <dbcksrb99@gmail.com>
2024-02-12 00:13:10 +0900

    feat : 6+강의까지 함!

--- Assets/Codes/Spawner.cs
+++ Assets/Codes/Spawner.cs
@@ -6,7 +6,9 @@ using UnityEngine.InputSystem;
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
+    public SpawnData[] spawnData;
 
+    int level;
     float timer;
 
     private void Awake()
@@ -16,8 +18,9 @@ public class Spawner : MonoBehaviour
     private void Update()
     {
         timer += Time.deltaTime;
+        level = Mathf.FloorToInt(GameManager.instance.gameTime / 10f);
 
-        if (timer > 0.2f)
+        if (timer > spawnData[level].spawnTime)
         {
             timer = 0;
             Spawn();
@@ -26,8 +29,18 @@ public class Spawner : MonoBehaviour
 
     void Spawn()
     {
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0,2));
+        GameObject enemy = GameManager.instance.pool.Get(0);
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+        enemy.GetComponent<Enemy>().Init(spawnData[level]);
     }
 
+}
+
+[System.Serializable]
+public class SpawnData
+{
+    public float spawnTime;
+    public int spriteType;
+    public int health;
+    public float speed;
 }
